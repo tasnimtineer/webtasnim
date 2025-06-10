f9a985d90b96238e23554232e882ff4b4aa85bfe
tasnimtineer <tineertasnim@gmail.com>
2025-06-11 01:48:27 +0300

    add main.py

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
 from fastapi import FastAPI
-from fastapi.responses import HTMLResponse
 
 app = FastAPI()
 
-@app.get("/", response_class=HTMLResponse)
-async def welcome():
-    return "<h1>مرحبا تسنيم!</h1>"
+@app.get("/")
+def read_root():
+    return {"message": "مرحبًا تسنيم"}
