835a3ca8f17fd3f65db22cc974dfe88e9ab0a6d7
tasnimtineer <tineertasnim@gmail.com>
2025-06-11 17:48:22 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
 from flask import Flask
 
-app = Flask(_name_)
+app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "Hello  انا تسنيم احب القراءة والبرمجه!"
+    return "Hello to my world!"
 
-if _name_ == 'main':
+if __name__ == '_main_':
     app.run()
