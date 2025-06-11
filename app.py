dad20d8026cf0c93a84749170d9e4fd5bd1995f2
tasnimtineer <tineertasnim@gmail.com>
2025-06-11 17:55:07 +0300

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,10 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "Hello to my world!"
+    return "Hello to my world Let's go on a little tour and learn more about me!
+I love reading books and can finish a whole book in just three days.
+I'm passionate about programming and enjoy growth and trying new things.
+I'm brave and always try to face challenges, even when I'm afraid.!"
 
 if __name__ == '_main_':
     app.run()
